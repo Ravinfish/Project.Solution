453063f5e3bc2f6ccb170a7081debb55c25fa02c
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-04-30 11:15:22 -0700

    Complete Category controller and index view

--- ClassFinder/Controllers/CategoriesController.cs
+++ ClassFinder/Controllers/CategoriesController.cs
@@ -5,12 +5,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Net.Http.Headers;
 
 namespace ClassFinder.Controllers
 {
   public class CategoriesController : Controller
   {
-    private readonly ClassFinderContext _db; 
+    private readonly ClassFinderContext _db;
     public CategoriesController(ClassFinderContext db)
     {
       _db = db;
@@ -21,32 +22,92 @@ namespace ClassFinder.Controllers
       return View(model);
     }
 
-    public ActionResult Create ()
+    public ActionResult Create()
     {
       return View();
     }
 
     [HttpPost]
-    public ActionResult Create(Category Category)
+    public ActionResult Create(Category category)
     {
-      _db.Categories.Add(Category);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
+      if (ModelState.IsValid)
+      {
+        _db.Categories.Add(category);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+      return View(category); // Return to the create view with validation errors
     }
 
     public ActionResult Details(int id)
     {
-      Category thisCategory = _db.Categories
-      .Include(Category => Category.Companies)
-      .FirstOrDefault(Category => Category.CategoryId == id);
+      var thisCategory = _db.Categories
+          .Include(c => c.Companies)
+          .FirstOrDefault(c => c.CategoryId == id);
+
+      if (thisCategory == null)
+      {
+        return NotFound();
+      }
+
       return View(thisCategory);
     }
 
     public ActionResult Edit(int id)
     {
-      
+      var category = _db.Categories.FirstOrDefault(c => c.CategoryId == id);
+
+      if (category == null)
+      {
+        return NotFound();
+      }
+
+      return View(category);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Category category)
+    {
+      if (ModelState.IsValid)
+      {
+        _db.Categories.Update(category);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+
+      return View(category); // Return to the edit view with validation errors
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var category = _db.Categories.FirstOrDefault(c => c.CategoryId == id);
+
+      if (category == null)
+      {
+        return NotFound();
+      }
+
+      return View(category);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var category = _db.Categories.FirstOrDefault(c => c.CategoryId == id);
+
+      if (category == null)
+      {
+        return NotFound();
+      }
+
+      _db.Categories.Remove(category);
+      _db.SaveChanges();
+
+      return RedirectToAction("Index");
+
     }
   }
 }
 
-  
+
+
